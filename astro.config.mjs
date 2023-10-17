e85dcd278633b52d3787b3dc7052d040dc34785b
xrr2016 <xiaoranran1993@outlook.com>
2023-10-17 22:59:27 +0800

    feat: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  integrations: [react(), tailwind()],
+});
---
+++ src/components/Button.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const Button = ({ label }: {label: string}) => {
+  return <button>{label}</button>
+}
